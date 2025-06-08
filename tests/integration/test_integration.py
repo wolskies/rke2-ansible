d9b29e152a4a5585784dfa899993ccd92e7ed425
ed <ed.wolski2@gmail.com>
2025-06-08 11:40:13 -0400

    major revision - looks more lika a collection, added version check

---
+++ tests/integration/test_integration.py
@@ -0,0 +1,17 @@
+"""Tests for molecule scenarios."""
+
+from __future__ import absolute_import, division, print_function
+
+from pytest_ansible.molecule import MoleculeScenario
+
+
+def test_integration(molecule_scenario: MoleculeScenario) -> None:
+    """Run molecule for each scenario.
+
+    Args:
+        molecule_scenario: The molecule scenario object
+    Raises:
+        AssertionError: If the molecule scenario test does not return a zero exit code.
+    """
+    proc = molecule_scenario.test()
+    assert proc.returncode == 0
---
+++ tests/unit/test_basic.py
@@ -0,0 +1,10 @@
+"""Unit tests for wolskinet.rke2_ansible."""
+
+
+def test_basic() -> None:
+    """Dummy unit test that always passes.
+
+    Raises:
+        AssertionError: If the assertion fails.
+    """
+    assert bool(1) is True
